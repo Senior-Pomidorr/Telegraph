f2999f5aa5d2fc05e762b5c282502c4e98305ce5
«Daniil <danilkulikovskiy@gmail.com>
2023-06-05 00:02:09 +0300

    fix design cells

--- Telegraph/CustomCell/CustomTableViewCell.swift
+++ Telegraph/CustomCell/CustomTableViewCell.swift
@@ -20,7 +20,8 @@ class CustomTableViewCell: UITableViewCell {
   
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+       
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
